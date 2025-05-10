e515ef7f5e12a7338e5b71a76e8e7e6d0224d113
Wellington Pereira <43380060+wellingtonengps@users.noreply.github.com>
2025-05-10 18:00:55 -0300

    add services collections

---
+++ src/collections/Services.ts
@@ -0,0 +1,41 @@
+import type { CollectionConfig } from 'payload'
+
+const Services: CollectionConfig = {
+  slug: 'services',
+  access: {
+    read: () => true,
+  },
+  labels: {
+    singular: 'Serviço',
+    plural: 'Serviços',
+  },
+  admin: {
+    useAsTitle: 'titulo',
+  },
+  fields: [
+    {
+      name: 'titulo',
+      label: 'Título',
+      type: 'text',
+      required: true,
+    },
+    {
+      name: 'icone',
+      label: 'Ícone',
+      type: 'text',
+      required: true,
+      admin: {
+        description:
+          'Ex: "fa-solid fa-heart" ou o nome de um ícone da biblioteca que você estiver usando.',
+      },
+    },
+    {
+      name: 'descricao',
+      label: 'Descrição',
+      type: 'textarea',
+      required: true,
+    },
+  ],
+}
+
+export default Services
